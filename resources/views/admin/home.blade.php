77ed209e5a438787b9f6344f5ebfc5e3fb5c98bf
urlastway <andriy.semenchenko13@gmail.com>
2022-07-24 04:29:00 +0200

    finito

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Visualizza tutti i posts</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function(){
         Route::get('/home', 'HomeController@index')->name('home');
+        Route::resource('posts', 'PostController');
     });
 
 //Front office

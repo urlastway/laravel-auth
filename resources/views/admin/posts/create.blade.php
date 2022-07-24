75dbc1898a6f48812be1bf78efb841fb42cf411c
urlastway <andriy.semenchenko13@gmail.com>
2022-07-24 04:29:11 +0200

    finito

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                <h1>Crea un nuovo post</h1>
+                <h1>Create new post</h1>
             </div>
             <div class="card-body">
                 <form action="{{route('admin.posts.store')}}" method="POST">
@@ -25,7 +25,7 @@
                     </div>
                     <div class="form-group form-check">
                         <input type="checkbox" class="form-check-input @error('published') is-invalid @enderror" id="published" name="published" {{old('published') ? 'checked' : ''}}>
-                        <label class="form-check-label" for="published">Pubblica</label>
+                        <label class="form-check-label" for="published">Pubblic</label>
                         @error('published')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
